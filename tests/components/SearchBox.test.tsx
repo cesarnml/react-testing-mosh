a49e82edff3c765c0f069c1bcaa078b10c7174c6
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2024-03-21 21:24:11 +0700

    refactor lesson 19

--- tests/components/SearchBox.test.tsx
+++ tests/components/SearchBox.test.tsx
@@ -1,44 +1,47 @@
 import SearchBox from '@/components/SearchBox'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import exp from 'constants'
-
-const user = userEvent.setup()
 
 describe('SearchBox', () => {
-  it('should render an empty search input with placeholder text', () => {
-    const placeholder = 'Search...'
+  const setup = () => {
     const onChange = vi.fn()
+    const user = userEvent.setup()
 
     render(<SearchBox onChange={onChange} />)
     const input = screen.getByRole('textbox')
+
+    return {
+      user,
+      onChange,
+      input,
+    }
+  }
+  it('should render an empty search input with placeholder text', () => {
+    const placeholder = 'Search...'
+
+    const { input } = setup()
 
     expect(input).toHaveDisplayValue('')
     expect(input).toHaveAttribute('placeholder', placeholder)
   })
 
   it('should call onChange on "Enter" if input has non-empty value', async () => {
-    const onChange = vi.fn()
-
-    render(<SearchBox onChange={onChange} />)
     const text = 'Hello, World!'
-    const input = screen.getByRole('textbox')
+    const { input, user, onChange } = setup()
 
     await user.type(input, text)
 
     expect(input).toHaveDisplayValue(text)
     expect(onChange).not.toHaveBeenCalled()
 
-    await user.keyboard('{enter}')
+    await user.type(input, '{enter}')
 
     expect(onChange).toHaveBeenCalled()
     expect(onChange).toHaveBeenLastCalledWith(text)
   })
 
   it('should not call onChange on "Enter" if input has empty value', async () => {
-    const onChange = vi.fn()
-
-    render(<SearchBox onChange={onChange} />)
+    const { user, onChange } = setup()
 
     expect(onChange).not.toHaveBeenCalled()
     await user.keyboard('{enter}')
