5073b40f2465d365fc287ed79114a11008084dbc
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2024-03-21 17:03:34 +0700

    minor

--- tests/components/TermsAndConditions.test.tsx
+++ tests/components/TermsAndConditions.test.tsx
@@ -3,6 +3,7 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
 const user = userEvent.setup()
+
 describe('TermsAndConditions', () => {
   beforeEach(() => {
     render(<TermsAndConditions />)
